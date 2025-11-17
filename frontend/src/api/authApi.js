e0464ad623a040e87cc246139ce263fe2be4a4dd
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-17 22:43:39 +0100

    fix: export all api functions

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,13 +1,13 @@
 import api from "./axiosClient"
 
-function register(data) {
+export function register(data) {
     return api.post("/auth/register", data)
 }
 
-function login(data) {
+export function login(data) {
     return api.post("/auth/login", data)
 }
 
-function logout() {
+export function logout() {
     return api.post("/auth/logout")
 }
