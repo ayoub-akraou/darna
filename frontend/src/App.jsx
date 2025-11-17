41f8d0e9ca616bdb70167a0db4582cccb55cd2f2
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-17 20:56:34 +0100

    style: link tailwindcss with react correctely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import './App.css'
 // import Login from './pages/auth/Login.jsx'
 import Register from './pages/auth/Register.jsx'
 
