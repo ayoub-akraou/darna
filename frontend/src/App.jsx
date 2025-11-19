ca1f75db935de20564a22c7ac166268b07df2c3f
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-19 10:00:00 +0100

    feat(routes): add the main dashboard layout route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
+import GroupsDashboard from "./pages/Group.jsx";
+import DashBordLayout from "./Layouts/DashBordLayout.jsx";
 import { Toaster } from "sonner";
 
 function App() {
@@ -14,6 +16,9 @@ function App() {
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/" element={<DashBordLayout />}>
+					<Route index element={<GroupsDashboard />} />
+				</Route>
 			</Routes>
 		</Router>
 		</>
