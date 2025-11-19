7e6f8e3ad2a8b3b7d5b6d7e7670d8f0d7c44cbe3
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-19 09:54:55 +0100

    feat(auth): create LogoutBtn and implement logout functionality

---
+++ frontend/src/components/Button/LogoutBtn.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../api/authApi";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+import { toast } from "sonner";
+
+export default function LogoutBtn() {
+	const navigate = useNavigate();
+	const handleLogout = async () => {
+		try {
+			const response = await logout();
+			localStorage.removeItem("token");
+			// navigate("/login");
+			toast.success("Logout successful");
+		} catch (error) {
+			console.log(error);
+			toast.error("Logout failed");
+		}
+	};
+	return (
+		<button
+			onClick={handleLogout}
+			className="flex items-center gap-2 text-red-600 hover:text-red-700 transition-colors"
+		>
+			<ArrowRightOnRectangleIcon className="w-5 h-5" />
+			<span className="hidden sm:inline">Logout</span>
+		</button>
+	);
+}
