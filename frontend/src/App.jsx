1d99d05357e5629c24478991df5b5b319f301aeb
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-19 10:17:01 +0100

    refactor: rename GroupsDashboard to Group

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
-import GroupsDashboard from "./pages/Group.jsx";
+import Group from "./pages/Group.jsx";
 import MainLayout from "./Layouts/MainLayout.jsx";
 import { Toaster } from "sonner";
 
@@ -15,7 +14,7 @@ function App() {
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Register />} />
 					<Route path="/" element={<MainLayout />}>
-						<Route index element={<GroupsDashboard />} />
+						<Route index element={<Group />} />
 					</Route>
 				</Routes>
 			</Router>
