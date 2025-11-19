0b7ab3b5aa63d6fc35a2f1c1a58991d3cbba1349
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-19 09:57:04 +0100

    feat(App): configure sonner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
+import { Toaster } from "sonner";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
+		<>
+		<Toaster />
 		<Router>
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 			</Routes>
 		</Router>
+		</>
 	);
 }
 
